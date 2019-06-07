7f779801dcc688467c0b0f19af74adcddf822bea
Gabriel Diaz <g.diaz.lopezllave@bbva.com>
2019-06-07 12:26:59 +0200

    Update spec library to support testing in benchmarks

--- testutils/spec/spec.go
+++ testutils/spec/spec.go
@@ -54,35 +54,35 @@ func New() (LetF, ReportF) {
 		}
 }
 
-func Equal(t *testing.T, exp, got interface{}, msg string) {
+func Equal(t testing.TB, exp, got interface{}, msg string) {
 	t.Helper()
 	if !reflect.DeepEqual(exp, got) {
 		t.Fatalf("Not equals: %s -> expecting '%v' got '%v'\n", msg, exp, got)
 	}
 }
 
-func True(t *testing.T, cond bool, msg string) {
+func True(t testing.TB, cond bool, msg string) {
 	t.Helper()
 	if cond != true {
 		t.Fatalf("Condition is not true: %s -> %v", msg, cond)
 	}
 }
 
-func False(t *testing.T, cond bool, msg string) {
+func False(t testing.TB, cond bool, msg string) {
 	t.Helper()
 	if cond != false {
 		t.Fatalf("Condition is not false: %s -> %v", msg, cond)
 	}
 }
 
-func NoError(t *testing.T, err error, msg string) {
+func NoError(t testing.TB, err error, msg string) {
 	t.Helper()
 	if err != nil {
 		t.Fatalf("Error is not nil: %s -> %v", msg, err)
 	}
 }
 
-func Error(t *testing.T, err error, msg string) {
+func Error(t testing.TB, err error, msg string) {
 	t.Helper()
 	if err == nil {
 		t.Fatalf("Error is not nil: %s -> %v", msg, err)
@@ -104,7 +104,7 @@ func isNil(object interface{}) bool {
 	return false
 }
 
-func NotNil(t *testing.T, object interface{}, msg string) {
+func NotNil(t testing.TB, object interface{}, msg string) {
 	t.Helper()
 	if isNil(object) {
 		t.Fatalf("Object is nil: %v --> %v", msg, object)
